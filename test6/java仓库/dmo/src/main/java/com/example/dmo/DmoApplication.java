989cb308f26200cac675347d4c18e1ad8840902a
waylon123254 <114789672+waylon123254@users.noreply.github.com>
2023-12-07 15:10:57 +0800

    Add files via upload

---
+++ test6/java仓库/dmo/src/main/java/com/example/dmo/DmoApplication.java
@@ -0,0 +1,13 @@
+package com.example.dmo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DmoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DmoApplication.class, args);
+    }
+
+}
---
+++ test6/java仓库/dmo/src/test/java/com/example/dmo/DmoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dmo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DmoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
