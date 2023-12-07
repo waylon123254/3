d356c40f6ecb1ba11a53e1bde6bf45e483db89ff
waylon123254 <114789672+waylon123254@users.noreply.github.com>
2023-12-07 15:04:33 +0800

    Add files via upload

---
+++ test6/java仓库/9/src/main/java/dao/UserDao.java
@@ -0,0 +1,5 @@
+package dao;
+import pojo.User;
+public interface UserDao {
+    void add(User user);
+}
---
+++ test6/java仓库/9/src/main/java/dao/UserDaoImpl.java
@@ -0,0 +1,22 @@
+package dao;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+import pojo.User;
+
+@Repository
+public class UserDaoImpl implements UserDao{
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+    //添加的方法
+    @Override
+    public void add(User user) {
+        //  1、创建sql语句
+        String sql = "insert into 嗨害嗨 values(?,?,?)";
+        //  2、调用方法实现
+        Object[] args = {user.getId(), user.getName(), user.getAge()};
+        int update = jdbcTemplate.update(sql, args);
+        System.out.println(update);
+    }
+}
